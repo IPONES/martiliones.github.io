cf6aac88c7b0a244dfcc5d7b6501f81bbe60c876
martiliones <slomes@yandex.kz>
2017-10-14 14:56:48 +0700

    Add files via upload

---
+++ src/views/Post.vue
@@ -0,0 +1,37 @@
+<template>
+  
+  <div  class="container">
+    <div  class="row">
+      <div  class="cardPost" hover="">
+        <div  class="post__img unkown">
+          <img  :src="post.img" class="post__img imgP">
+          <span class="card-titlePost">{{ post.name }}</span>
+        </div>
+      <div  class="card-contentPost">
+        <p>{{ post.fullDescription }}</p>
+      </div>
+      <div class="card-actionPost">
+        <router-link to="/"  style="cursor: pointer" class="card-txt">Назад</router-link>
+      </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  var posts = require('../posts')
+
+  module.exports = {
+    data: function() {
+      return {
+        posts: posts,
+        post: null
+      }
+    },
+    created: function() {
+      var postId = this.$route.params.id
+
+      this.post = this.posts[postId]
+    }
+  }
+</script>
---
+++ src/views/addPost.vue
@@ -0,0 +1,65 @@
+<template>
+	<div style="display:block;">
+		<input 
+			type="text"
+			placeholder="Название нового поста"
+			class="search__input"
+			v-model="namePost"/>
+
+		<input 
+			type="text"
+			placeholder="Изображение"
+			class="search__input"
+			style="width:70%;" 
+			v-model="image"/>
+
+
+		<textarea
+			placeholder="Полное описание"
+			class="search__input textarea"
+			v-model="fullDesc">
+		</textarea> 
+		
+		<p class="limit">{{ fullDesc.length }} / 1301</p>
+		
+
+		<p 	v-if="fullDesc.length < 1301" 
+			class="card-txt save__btn"
+			@click="addNewPost">
+			
+			Сохранить
+		</p>
+	</div>
+</template>
+
+<script>
+	var posts = require('../posts')
+
+	module.exports = {
+		data: function() {
+	  		return {
+	  			posts: posts,
+	  			fullDesc:'',
+	  			namePost:'',
+	  			image:'',
+	  			nextTODO:3
+      		}
+    	},
+    	methods: {
+		    addNewPost: function () {
+		   	    posts.push({
+		        	fullDescription: this.fullDesc,
+		        	name: this.namePost,
+		        	searchName: this.namePost,
+		        	img: this.image,
+		        	id: this.nextTODO
+		    	})
+		     	this.fullDesc = ''
+		    	this.namePost= ''
+		      	this.image = ''
+		      	this.nextTODO++
+		    }
+		 }
+   	}
+	
+</script>
