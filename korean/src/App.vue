4d114912a95927cec7da65a16ec79e712d6cb3b6
martiliones <slomes@yandex.kz>
2018-02-11 16:03:14 +0600

    Add files via upload

---
+++ korean/src/App.vue
@@ -0,0 +1,86 @@
+<template>
+  <div id="app">
+    <div class="site_name">아름다움</div>
+	
+	  <div class="row">
+	    <div class="col">
+	      <img src="http://www.iloveasia.su/wp-content/uploads/2015/09/tAOXKUu.jpg" alt="">
+	      <div class="name">
+	      	Это кореяц
+	      </div>
+	    </div>
+	    <div class="col">
+	      <img src="http://www.iloveasia.su/wp-content/uploads/2015/09/tAOXKUu.jpg" alt="">
+	      <div class="name">
+	      	Это кореяц
+	      </div>
+	    </div>
+	    <div class="col">
+	      <img src="http://www.iloveasia.su/wp-content/uploads/2015/09/tAOXKUu.jpg" alt="">
+	      <div class="name">
+	      	Это кореяц
+	      </div>
+	    </div>
+	  </div>
+
+	  <div class="row">
+	    <div class="col">
+	      <img src="http://www.iloveasia.su/wp-content/uploads/2015/09/tAOXKUu.jpg" alt="">
+	      <div class="name">
+	      	Это кореяц
+	      </div>
+	    </div>
+	    <div class="col">
+	      <img src="http://www.iloveasia.su/wp-content/uploads/2015/09/tAOXKUu.jpg" alt="">
+	      <div class="name">
+	      	Это кореяц
+	      </div>
+	    </div>
+	    <div class="col">
+	      <img src="http://www.iloveasia.su/wp-content/uploads/2015/09/tAOXKUu.jpg" alt="">
+	      <div class="name">
+	      	Это кореяц
+	      </div>
+	    </div>
+	  </div>
+
+	  <div class="row">
+	    <div class="col">
+	      <img src="http://www.iloveasia.su/wp-content/uploads/2015/09/tAOXKUu.jpg" alt="">
+	      <div class="name">
+	      	Это кореяц
+	      </div>
+	    </div>
+	    <div class="col">
+	      <img src="http://www.iloveasia.su/wp-content/uploads/2015/09/tAOXKUu.jpg" alt="">
+	      <div class="name">
+	      	Это кореяц
+	      </div>
+	    </div>
+	    <div class="col">
+	      <img src="http://www.iloveasia.su/wp-content/uploads/2015/09/tAOXKUu.jpg" alt="">
+	      <div class="name">
+	      	Это кореяц
+	      </div>
+	    </div>
+	  </div>
+
+
+
+  </div>
+</template>
+
+<script>
+import App from './App.vue'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style src="./assets/style.css"></style>
