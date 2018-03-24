16b92857b58de59b3b130a1c30359760a3bc059b
martiliones <slomes@yandex.kz>
2018-03-24 16:05:05 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 module.exports = function() {
-  console.log("js")
+  fs.writeFile("C:/hello.txt", "Hello мир!", function(error){
+ 
+    if(error) throw error; // если возникла ошибка
+    console.log("Асинхронная запись файла завершена. Содержимое файла:");
+    var data = fs.readFileSync("hello.txt", "utf8");
+    console.log(data);  // выводим считанные данные
+  });
 }
