c7b10ec0f35286be40fa5c762fc7a5a8f9261025
martiliones <slomes@yandex.kz>
2018-03-24 16:12:21 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 var fs = require('fs');
 
 module.exports = function() {
-  fs.writeFile("C:/hello.txt", "Hello мир!", function(error){
+  fs.writeFile("./hello.txt", "Hello мир!", function(error){
  
     if(error) throw error; // если возникла ошибка
     console.log("Асинхронная запись файла завершена. Содержимое файла:");
-    var data = fs.readFileSync("C:/hello.txt", "utf8");
+    var data = fs.readFileSync("./hello.txt", "utf8");
     console.log(data);  // выводим считанные данные
   });
 }
